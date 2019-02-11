6705102bbef1b52343ccd011e6f1d56b7dd8d60d
annapancioli <annapancioli@gmail.com>
2019-02-11 13:05:39 -0500

    subscribe functional

--- admin/sign-up.php
+++ admin/sign-up.php
@@ -2,6 +2,7 @@
 
 function sendForm($fname,$lname,$email,$country){
     include('connect.php');
+    include('send_email.php');
 
     //check if email exists
     $check_email_query = 'SELECT subs_email FROM tbl_subs WHERE subs_email = :subs_email';
@@ -14,7 +15,7 @@ function sendForm($fname,$lname,$email,$country){
 
     $row = $email_set->fetch(PDO::FETCH_ASSOC);
 
-    //if($email_set -> fetch() > 0){
+    //if($email_set -> fetchColumn() > 0){
     if(!empty($row['subs_email'])){
         $update_subs_query = 'UPDATE tbl_subs SET subs_last_update = NOW(), subs_fname = :fname, subs_lname = :lname, subs_country = :country WHERE subs_email = :email';
         $update_subs_set = $pdo->prepare($update_subs_query);
@@ -27,15 +28,32 @@ function sendForm($fname,$lname,$email,$country){
             )
         );
 
-        /*$replace_email_query = 'REPLACE INTO tbl_subs(subs_fname,subs_lname,subs_email,subs_country,subs_last_update)';
+        return send_email($fname, $email, true);
+        exit;
+
+        //redirect_to('./index_subs.php');
+
+        //$result = 'Welcome back!';
+        //return $message;    
+
+        /*$replace_email_query = 'REPLACE INTO tbl_subs(subs_fname,subs_lname,subs_country,subs_last_update)';
         $replace_email_query .= ' VALUES(:fname,:lname,:email,:country,NOW())';
         $replace_email_set = $pdo->prepare($replace_email_query);
         $replace_email_set->execute(
             array(
                 ':fname'=>$fname,
                 ':lname'=>$lname,
-                ':email'=>$email,
                 ':country'=>$country
+            )
+        );*/
+
+        // updates the last login date and time
+        /*date_default_timezone_set('America/Toronto');
+        $update_date_query = 'UPDATE tbl_subs SET subs_date = NOW() WHERE subs_id = :id';
+        $update_date_set = $pdo->prepare($update_date_query);
+        $update_date_set->execute(
+            array(
+                ':id' => $id
             )
         );*/
 
@@ -52,18 +70,29 @@ function sendForm($fname,$lname,$email,$country){
                 ':lname'=>$lname,
                 ':email'=>$email,
                 ':country'=>$country
-            )
+            ) 
         );
 
-        // updates the last login date and time
-        /*date_default_timezone_set('America/Toronto');
-        $update_date_query = 'UPDATE tbl_subs SET subs_date = NOW() WHERE subs_id = :id';
-        $update_date_set = $pdo->prepare($update_date_query);
-        $update_date_set->execute(
-            array(
-                ':id' => $id
-            )
-        );*/
+        if($create_subs_set){
+			return send_email($fname, $email, false);
+		}else{
+			return 'Subscription failed';
+		}
+
+        //redirect_to('./index_subs.php');
+        
+        /*function sendEmail(){
+
+            $to = $email;
+            $subject = "Test mail 2";
+            $message = "Hello! This is a simple email message 2.";
+            $headers = "From: admin@admin.com";
+            
+        };*/
+
+        //$message = 'Welcome!';
+        //return $message;
+
     }
 
 }
